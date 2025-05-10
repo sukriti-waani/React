602c3fa8b152d507711f7a10026569e10d1f976d
Sukriti Waani <sukritiwaani2006@gmail.com>
2025-05-10 09:34:07 +0530

    Learned React Hooks and useState for state management; set up and used Vite for React project

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Greet from './Greet';
+
+function App() {
+  const username = "Hello World"
+  return (
+    <>
+      <Greet></Greet>
+      <h1>React {username}</h1>
+      <p>Testing</p>
+    </>
+    
+  )
+}
+
+export default App;
---
+++ 02counter/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(15);
+
+  // let counter = 15;
+
+  const addValue = () => {
+    counter = counter + 1;
+    setCounter(counter);
+    console.log("clicked", counter);
+  };
+
+  const removeValue = () => {
+    setCounter(counter - 1);
+  };
+
+  return (
+    <>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  );
+}
+
+export default App;
