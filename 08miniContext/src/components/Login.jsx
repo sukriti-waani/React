aa0f56067b3405688e8f82875c0e9ae2004adbce
Sukriti Waani <sukritiwaani2006@gmail.com>
2025-05-16 15:14:26 +0530

    Context API mini project with working login state management

---
+++ 08miniContext/src/components/Login.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useContext } from "react";
+import UserContext from "../context/UserContext";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { setUser } = useContext(UserContext); // Extracts the setUser function from the UserContext
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); //  prevent the page from reloading on form submission.
+    setUser({ username, password }); 
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="username"
+      />
+      {"  "}
+      <input
+        type="text"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="password"
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+};
+
+export default Login;
